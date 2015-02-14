545426dbcb9ffc5b8c435fd928fc37ea57c57e16
Xavier Defrang <xavier.defrang@gmail.com>
2015-02-15 00:39:54 +0100

    Added data directory to package

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Expletive.Mixfile do
 
   defp package do
     [
-     files: ["lib", "priv", "mix.exs", "README*", "LICENSE*"],
+     files: ["lib", "data", "priv", "mix.exs", "README*", "LICENSE*"],
      contributors: ["Xavier Defrang"],
      licenses: ["Apache 2.0"],
      links: %{"GitHub" => "https://github.com/xavier/expletive"}
