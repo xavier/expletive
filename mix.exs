75929c368b24b244260ea216dc64d939134626d9
Xavier Defrang <xavier.defrang@gmail.com>
2015-02-15 13:06:17 +0100

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Expletive.Mixfile do
 
   def project do
     [app: :expletive,
-     version: "0.1.2",
+     version: "0.1.3",
      elixir: "~> 1.0",
      deps: deps,
      description: description,
