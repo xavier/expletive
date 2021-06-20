3f0593ab86421e255a25c57a61e6cf3140b148ab
Xavier Defrang <xavier.defrang@gmail.com>
2021-06-20 12:23:40 +0200

    Ignore blank lines

--- lib/expletive/blacklist.ex
+++ lib/expletive/blacklist.ex
@@ -1,21 +1,19 @@
 defmodule Expletive.Blacklist do
-
-  strip_quotes = fn (word) -> String.replace(word, ~r/^"\s*|\s*"$/, "") end
+  strip_quotes = fn word -> String.replace(word, ~r/^"\s*|\s*"$/, "") end
 
   data_dir = Path.join([__DIR__, "..", "..", "data", "*.txt"])
 
   for file <- Path.wildcard(data_dir) do
+    fun_name = Path.basename(file, ".txt") |> String.to_atom()
 
-    fun_name = Path.basename(file, ".txt") |> String.to_atom
-
-    words = File.read!(file)
-            |> String.split("\n")
-            |> Enum.map(strip_quotes)
+    words =
+      File.read!(file)
+      |> String.split("\n")
+      |> Stream.map(&String.trim/1)
+      |> Stream.filter(fn line -> line != "" end)
+      |> Enum.map(strip_quotes)
 
     @doc "Returns #{fun_name} words to blacklist"
     def unquote(fun_name)(), do: unquote(words)
-
   end
-
 end
-
