0478171d54052a9a1703d97f9af86f516cd81203
Xavier Defrang <xavier.defrang@gmail.com>
2021-06-20 14:03:58 +0200

    Add formatter configuration

--- mix.exs
+++ mix.exs
@@ -2,14 +2,16 @@ defmodule Expletive.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :expletive,
-     version: "0.1.4",
-     elixir: "~> 1.0",
-     deps: deps(),
-     description: description(),
-     package: package(),
-     source_url: "https://github.com/xavier/expletive",
-     homepage_url: "https://github.com/xavier/expletive"]
+    [
+      app: :expletive,
+      version: "0.1.4",
+      elixir: "~> 1.0",
+      deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/xavier/expletive",
+      homepage_url: "https://github.com/xavier/expletive"
+    ]
   end
 
   # Configuration for the OTP application
@@ -29,8 +31,7 @@ defmodule Expletive.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:earmark, "~> 0.1", only: :dev},
-     {:ex_doc, "0.6.0", only: :dev}]
+    [{:earmark, "~> 0.1", only: :dev}, {:ex_doc, "0.6.0", only: :dev}]
   end
 
   defp description do
@@ -39,10 +40,10 @@ defmodule Expletive.Mixfile do
 
   defp package do
     [
-     files: ["lib", "data", "priv", "mix.exs", "README*", "LICENSE*"],
-     contributors: ["Xavier Defrang"],
-     licenses: ["Apache 2.0"],
-     links: %{"GitHub" => "https://github.com/xavier/expletive"}
-   ]
+      files: ["lib", "data", "priv", "mix.exs", "README*", "LICENSE*"],
+      contributors: ["Xavier Defrang"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/xavier/expletive"}
+    ]
   end
 end
