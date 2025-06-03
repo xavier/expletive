9e05802bd3b67d921021c9e6ece482966c977788
Xavier Defrang <xavier.defrang@gmail.com>
2025-06-03 17:26:27 +0200

    Version 0.1.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Expletive.Mixfile do
   def project do
     [
       app: :expletive,
-      version: "0.1.5",
+      version: "0.1.6",
       elixir: "~> 1.0",
       deps: deps(),
       description: description(),
